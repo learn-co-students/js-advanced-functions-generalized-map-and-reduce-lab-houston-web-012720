96b45f0bf7ffc0dce0f1e7db41c2a4399602e67a
somaia-khalil <sumaya_khair@live.com>
2020-03-21 13:17:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+
+function map(array, callback) {
+    let newarray = []
+    for (let i = 0; i < array.length; i++) {
+        newarray.push(callback(array[i]))
+    }
+    return newarray
+}
+
+function reduce(array, callback, startpoint) {
+    let acc = startpoint
+    if (startpoint) {
+        for (let i = 0; i < array.length; i++) {
+            acc = callback(acc, array[i])
+        }
+    } else {
+        acc = array[0]
+        for (let i = 1; i < array.length; i++) {
+            acc = callback(acc, array[i])
+        }
+    }
+    return acc
+}
